5f953d9822178cd718c7081102c1eb4b2ff54636
ianam <iana.markevitch@gmail.com>
2021-06-11 14:49:35 -0700

    Working application form

--- email-application.php
+++ email-application.php
@@ -1,76 +1,69 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-error_reporting(-1);
-ini_set('display_errors', 'On');
-set_error_handler("var_dump");
+require 'PHPMailer/src/Exception.php';
+require 'PHPMailer/src/PHPMailer.php';
 
-if(!isset($_POST['submit']))
-{
-    echo "The form needs to be submitted";
-}
-    $fname = $_POST['fname'];
-    $lname = $_POST['lname'];
-    $tel = $_POST['tel'];
-    $usr_email = $_POST['email'];
-    $field_exp = $_POST['field'];
-    $yrs_exp = $_POST['yrs-exp'];
-    $desig = $_POST['desig'];
-    $elig = $_POST['elig'];
-    $avail = $_POST['avail'];
+if (array_key_exists('to', $_POST)) {
+    $err = false;
+    $msg = '';
+    $email = '';
 
-    // Validate against email injection attempts
-    if(IsInjected($usr_email))
-    {
-        echo "Invalid email value";
-        exit;
+    // Basic validations: first name, last name
+    if (array_key_exists('fname', $_POST)) {
+        $fname = substr(strip_tags($_POST['fname']), 0, 255);
+    } else {
+        $fname = '';
+    }
+
+    if (array_key_exists('lname', $_POST)) {
+        $lname = substr(strip_tags($_POST['lname']), 0, 255);
+    } else {
+        $lname = '';
+    }
+
+    // Email validation
+    if (array_key_exists('email', $_POST) && PHPMailer:validateAddress($_POST['email'])) {
+        $email = $_POST['email'];
+    } else {
+        $msg .= "Error: invalid email address provided";
+        $err = true;
     }
 
     // Retrieve checkbox list
     $select_types = 'None';
     if(isset($_POST['type']) && is_array($_POST['type']) && count($_POST['type']) > 0){
         $select_types = implode(', ', $_POST['type']);
+
+    $tel = $_POST['tel'];
+    $field_exp = $_POST['field'];
+    $yrs_exp = $_POST['yrs-exp'];
+    $desig = $_POST['desig'];
+    $elig = $_POST['elig'];
+    $avail = $_POST['avail'];
+
+    if (!err) {
+        $mail = new PHPMailer();
+        $mail->SetFrom('careers@healthstaffing.ca', 'Health Staffing');
+        $mail->addAddress('careers@healthstaffing.ca');
+        $mail->addReplyTo($email);
+        $mail->Subject = 'Application form submission from Healthstaffing.ca';
+        $mail->Body = "Application Form submission from Healthstaffing.ca\n".
+                        "First Name: $fname\n".
+                        "Last Name: $lname\n".
+                        "Phone: $tel\n".
+                        "Email: $email\n".
+                        "Field of experience: $field_exp\n".
+                        "Years of experience: $yrs_exp\n".
+                        "Designation: $desig\n".
+                        "Eligible to work in Canada: $elig\n".
+                        "Availability start date: $avail\n".
+                        "Employment type: " . $select_types;
+        if (!mail->send()) {
+            $msg .= 'Mailer Error: ' . $mail->ErrorInfo;
+        } else {
+            $msg .= 'Message sent!';
+        }
     }
-
-    $email_from = 'careers@healthstaffing.ca';
-    $email_subject = "New application form submission from Healthstaffing.ca";
-    $email_body = "Application Form submission from Healthstaffing.ca\n".
-                    "First Name: $fname\n".
-                    "Last Name: $lname\n".
-                    "Phone: $tel\n".
-                    "Email: $usr_email\n".
-                    "Field of experience: $field_exp\n".
-                    "Years of experience: $yrs_exp\n".
-                    "Designation: $desig\n".
-                    "Eligible to work in Canada: $elig\n".
-                    "Availability start date: $avail\n".
-                    "Employment type: " . $select_types;
-
-    $to = 'iana.markevitch@gmail.com';
-    $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $usr_email \r\n";
-
-    mail($to,$email_subject,$email_body,$headers);
-
-    // Validation function
-    function IsInjected($str)
-    {
-        $injections = array('(\n+)',
-                    '(\r+)',
-                    '(\t+)',
-                    '(%0A+)',
-                    '(%0D+)',
-                    '(%08+)',
-                    '(%09+)'
-        );
-        $inject = join('|', $injections);
-        $inject = "/$inject/i";
-        if(preg_match($inject,$str))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-    }
-?>
+} ?>
