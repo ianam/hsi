074185e1f5a8b289c223ed4232cdd8d98dfe5e2b
ianam <iana.markevitch@gmail.com>
2022-03-31 12:40:34 -0700

    Mass update

--- email-contact.php
+++ email-contact.php
@@ -7,11 +7,28 @@ use PHPMailer\PHPMailer\SMTP;
 require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
+require 'recaptcha/src/autoload.php';
+
+$DatabaseAccess = parse_ini_file('../access.ini');
 
 if (array_key_exists('message', $_POST)) {
     $err = false;
     $msg = '';
     $email = '';
+    $captcha = '';
+
+    // Check captcha response
+    if (isset($_POST["g-recaptcha-response"]))
+        $captcha = $_POST["g-recaptcha-response"];
+
+    if ($captcha == '')
+        $err = true; 
+    $secret = '6LfpqOweAAAAAA7LYPfEjtR40n5pb-EMzVOHIBD7';
+    $response = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secret."&response=".$captcha."&remoteip=".$_SERVER["REMOTE_ADDR"]), true);
+
+    if ($response["success"] = false) {
+        $err = true;
+    }
 
     // Server-side validations
     if (array_key_exists('fname', $_POST)) {
@@ -57,22 +74,68 @@ if (array_key_exists('message', $_POST)) {
 
         $mail->isSMTP();
         $mail->SMTPDebug = SMTP::DEBUG_LOWLEVEL;
-        $mail->Host = 'smtp.gmail.com';
-        $mail->Port = 465;
+        $mail->Host = $DatabaseAccess['host'];
+        $mail->Port = $DatabaseAccess['port'];
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
         $mail->SMTPAuth = true;
-        $mail->Username = 'info@healthstaffing.ca';
-        $mail->Password = 'hs$2021van';
+        $mail->Username = $DatabaseAccess['username'];
+        $mail->Password = $DatabaseAccess['password'];
         $mail->CharSet = PHPMailer::CHARSET_UTF8;
 
         $mail->setFrom('info@healthstaffing.ca', 'Contact Form');
         $mail->addAddress('info@healthstaffing.ca');
-        $mail->addReplyTo($email, $fname . $lname);
+        $mail->addReplyTo($email, $fname . " " . $lname);
 
         $mail->Subject = 'New contact form submission from healthstaffing.ca';
 
-        $mail->Body = "Contact form submission\n\n" . $message . $fname . $lname . $phone . $email;
-
+        $mail->isHTML(true);
+        $mail->Body = "
+        <html>
+            <head>
+                <style>
+                    div { margin: auto; max-width: 600px; margin-top: 50px; }
+                    section { background-color: #fff; padding-bottom: 50px; }
+                    h3 { background-color: #78b5c1; margin: 0; padding: 40px 20px; color: #fff; font-size: 30px; font-weight: 200; border: 1px solid #dcdcdc; border-radius: 5px; }
+                    p { margin: 0; padding: 20px 20px;}
+                    table { border: 1px solid #ddd; border-radius: 5px; padding: 5px; margin: 0 20px; }
+                    body { font-family: sans-serif; background-color: #f5f5f5; }
+                    th, td { padding: 10px 50px 10px 10px; }
+                    td:first-child { background-color: #78b5c1; color: #fff; border-radius: 5px; }
+                </style>
+            </head>
+            <body>
+                <div>
+                    <h3>Contact Form</h3>
+                    <section>
+                        <p>A contact form submission has been made from  healthstaffing.ca.</p>
+                        <table>
+                            <tr>
+                                <td>First name</td>
+                                <td>$fname</td>
+                            </tr>
+                            <tr>
+                                <td>Last name</td>
+                                <td>$lname</td>
+                            </tr>
+                            <tr>
+                                <td>Phone number</td>
+                                <td>$phone</td>
+                            </tr>
+                            <tr>
+                                <td>Email</td>
+                                <td>$email</td>
+                            </tr>
+                            <tr>
+                                <td>Message</td>
+                                <td>$message</td>
+                            </tr>
+                        </table>
+                    </section>
+                </div>
+            </body>
+        </html>
+        ";
+        
         if (!$mail->send()) {
             $msg .= 'Mailer Error: ' . $mail->ErrorInfo;
         } else {
--- index.js
+++ index.js
@@ -1,24 +1,39 @@
-// $(document).ready(function() {
-//     $('form').on('submit', function(e) {
-//         let dataString = $(this).serialize();
+// Captcha check
+function recaptchaCallback() {
+    $('#recaptcha-check').val(1);
+}
 
-//         $.ajax({
-//             type: 'POST',
-//             url: 'email-contact.php',
-//             data: dataString,
-//             success: function() {
+$(document).ready(function() {
+    // Feature section animation
+    $(".feature").each(function() {
+        rand = Math.random() * (800);
+        $(this).delay(rand).animate({ opacity: 1 }, 500);
+    })
 
-//                 $('#contact-form').html("<div id='message'></div>");
-//                 $('#message')
-//                     .html("<h2>Contact form submitted!</h2>")
-//                     .append("<p>We will be in touch soon.</p>")
-//                     .hide()
-//                     .fadeIn(1500, function() {
-//                         $('#message')
-//                     });
-//             }
-//         });
+    if (location.hash !== null && location.hash !== "") {
+        $(location.hash + ".collapse").collapse("show");
+    }
 
-//         e.preventDefault();
-//     });
-// });
+    // Form handling
+    $('form').on('submit', function(e) {
+        let dataString = $(this).serialize();
+
+        $.ajax({
+            type: 'POST',
+            url: 'email-contact.php',
+            data: dataString,
+            success: function() {
+                $('#contact-form').html("<div id='message'></div>");
+                $('#message')
+                    .html("<h2>Message sent!</h2>")
+                    .append("<p>Thank you - we will be in touch soon.</p>")
+                    .hide()
+                    .fadeIn(1500, function() {
+                        $('#message')
+                    });
+            }
+        });
+
+        e.preventDefault();
+    });
+});
